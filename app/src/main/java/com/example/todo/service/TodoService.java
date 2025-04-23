24394a1cff1977ea0beff4e03f934b90f3e46fb9
alice.b9 <alice.b9581@gmail.com>
2025-04-23 15:00:22 +0900

    add dependencies prometheus, grafana

--- app/src/main/java/com/example/todo/service/TodoService.java
+++ app/src/main/java/com/example/todo/service/TodoService.java
@@ -2,6 +2,7 @@ package com.example.todo.service;
 
 import com.example.todo.domain.Todo;
 import com.example.todo.repository.TodoRepository;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,14 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TodoService {
+    private final MeterRegistry meterRegistry;
     private final TodoRepository todoRepository;
 
     public List<Todo> findAll(String user) {
         return todoRepository.findByUsername(user);
     }
     public Todo save(Todo todo) {
+        meterRegistry.counter("todo.created").increment();
         return todoRepository.save(todo);
     }
     public void delete(Long id) {
